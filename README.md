7bd8d3d9fa6f35791811b2218a2369d9bf539ba5
Rhett Lowe <rng2ml@gmail.com>
2015-08-25 00:08:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# uci-node-ldap [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> Node.js LDAP search tool tailered for simple UCI directory searching
+
+
+## Install
+
+```sh
+$ npm install --save uci-node-ldap
+```
+
+
+## Usage
+
+```js
+var uciNodeLdap = require('uci-node-ldap');
+
+uciNodeLdap('Rainbow');
+```
+
+## License
+
+ © [Rhett Lowe](https://github.oit.uci.edu/rhett)
+
+
+[npm-image]: https://badge.fury.io/js/uci-node-ldap.svg
+[npm-url]: https://npmjs.org/package/uci-node-ldap
+[travis-image]: https://travis-ci.org/education/uci-node-ldap.svg?branch=master
+[travis-url]: https://travis-ci.org/education/uci-node-ldap
+[daviddm-image]: https://david-dm.org/education/uci-node-ldap.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/education/uci-node-ldap
+[coveralls-image]: https://coveralls.io/repos/education/uci-node-ldap/badge.svg
+[coveralls-url]: https://coveralls.io/r/education/uci-node-ldap
---
+++ test/index.js
@@ -0,0 +1,148 @@
+'use strict';
+
+var assert = require('assert');
+var ldap   = require('../lib');
+
+describe('function searchBy', function () {
+
+  it('should throw error when no arguments given', function () {
+
+    assert.throws(function () {
+      ldap.searchBy();
+    }, Error);
+
+  });
+  it('should throw error when first argument isn\'t string', function () {
+
+    assert.throws(function () {
+      ldap.searchBy(null);
+    }, Error);
+
+  });
+  it('should throw error when first argument, string is empty', function () {
+
+    assert.throws(function () {
+      ldap.searchBy('');
+    }, Error);
+
+  });
+  it('should return error when first argument, string is empty', function (done) {
+
+    ldap.searchBy('', function (err) {
+      assert(err);
+      done();
+    });
+
+  });
+  it('should return error when first argument, string is empty', function (done) {
+
+    ldap.searchBy('', function (err) {
+      assert(err);
+      done();
+    });
+
+  });
+  it('should call callback with successful data or error after completion', function (done) {
+
+    ldap.searchBy('(uid=rhett)', function () {
+
+      assert(true);
+      done();
+
+    });
+
+  });
+  it('should return Rhett when searching for him by UCINetID', function (done) {
+
+    ldap.searchBy('(uid=rhett)', function (err, data) {
+
+      assert.ifError(err);
+      assert(data.length, 1);
+      assert(data[0].uid, 'rhett');
+
+      done();
+
+    });
+
+  });
+
+});
+
+describe('function searchByNetID', function () {
+
+  it('should throw error when no arguments given', function () {
+
+    assert.throws(function () {
+      ldap.searchByNetID();
+    }, Error);
+
+  });
+  it('should not throw error when no callback given', function () {
+
+    assert.doesNotThrow(function () {
+      ldap.searchByNetID('rhett');
+    }, Error);
+
+  });
+  it('should not throw error when no callback given, with like', function () {
+
+    assert.doesNotThrow(function () {
+      ldap.searchByNetID('rhett', true);
+    }, Error);
+
+  });
+  it('should call callback with successful data or error after completion', function (done) {
+
+    ldap.searchByNetID('rhett', function () {
+
+      assert(true, true);
+      done();
+
+    });
+
+  });
+  it('should return Rhett when searching for him by UCINetID', function (done) {
+
+    ldap.searchByNetID('rhett', function (err, data) {
+
+      assert.ifError(err);
+      assert(data.length, 1);
+      assert(data[0].uid, 'rhett');
+
+      done();
+
+    });
+
+  });
+  it('should return Rhett when searching for him by UCINetID with like', function (done) {
+
+    ldap.searchByNetID('hett', true, function (err, data) {
+
+      assert.ifError(err);
+      assert(data.length > 0, true);
+      assert(
+        data
+          .filter(function (i) {
+            return i.uid === 'rhett';
+          })
+          .length,
+        1
+      );
+
+      done();
+
+    });
+
+  });
+  it('should return error when looking for NetID less than 4 chars', function (done) {
+
+    ldap.searchByNetID('het', true, function (err) {
+
+      assert(err);
+      done();
+
+    });
+
+  });
+
+});
